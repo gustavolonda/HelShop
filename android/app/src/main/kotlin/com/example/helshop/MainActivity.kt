76c2e40dc3659d5525de28b487539ff642aced0d
gustavolonda <gustavolonda@gmail.com>
2021-04-12 19:08:31 -0500

    Init Project

---
+++ android/app/src/main/kotlin/com/example/helshop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.helshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+    
+        primarySwatch: Colors.blue,
+      ),
+     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+      appBar: AppBar(
+          title: Text(widget.title),
+      ),
+      body:,
+      );
+  }
+}
